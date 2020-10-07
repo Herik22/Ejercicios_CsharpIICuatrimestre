64680431b7c96401277ce3b6a50785759735412b
Herik Arismendy <herikuribe@gmail.com>
2020-10-07 02:14:07 -0300

    Avance en  practica parcial, hasta la clase Gato

---
+++ Practica_Parcial_I/ParcticaParcial/Gato.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcticaParcial
+{
+    public class Gato : Mascota
+    {
+        #region CONSTRUCTORES
+
+        public Gato(string name , string raza) :base(name,raza)
+        { 
+        }
+
+        #endregion
+
+        #region METODOS
+
+        /// <summary>
+        /// Permite comparar varios objetos.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            bool rta = false;
+            if(obj is Gato)
+            {
+                rta = this == (Gato)obj;
+            }
+            return rta;
+        }
+
+        /// <summary>
+        /// Sobre escritutra que muestras toda la informacion del Gato.
+        /// </summary>
+        /// <returns></returns>
+        protected override string Ficha()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Gato - \n");
+            sb.AppendFormat(base.DatosCompletos());
+            sb.AppendLine();
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// retorna la ficha del gato.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.Ficha();
+        }
+        #endregion
+
+        #region SOBRECARGAS
+
+        /// <summary>
+        ///  TRUE = seran iguales si comparten Nombre,Raza
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns></returns>
+        public static bool operator == (Gato g1, Gato g2)
+        {
+            bool rta = false;
+            if ((Mascota)g1 == (Mascota)g2)
+            {
+                rta = true;
+            }
+            return rta;
+        }
+
+        public static bool operator != (Gato g1, Gato g2)
+        {
+            return !(p1 == p2);
+        }
+
+        #endregion
+    }
+}
---
+++ Practica_Parcial_I/ParcticaParcial/Mascota.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcticaParcial
+{
+    public abstract class Mascota
+    {
+        #region ATRIBUTOS
+        private string nombre;
+        private string raza;
+        #endregion
+
+        #region PROPIEDADES
+        /// <summary>
+        /// Propiedad de solo lectura para el atributo nombre 
+        /// </summary>
+        public string Nombre 
+        {
+            get
+            {
+                return this.nombre;
+            }
+         }
+
+        /// <summary>
+        ///  Propiedad de solo lectura para el atributo raza
+        /// </summary>
+        public string Raza
+        {
+            get
+            {
+                return this.raza;
+            }
+        }
+        #endregion
+
+        #region CONSTRUCTORES
+
+        /// <summary>
+        /// Unico constructor parametrizado.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="raza"></param>
+        public Mascota (string name , string raza)
+        {
+            this.nombre = name;
+            this.raza = raza;
+        }
+
+        #endregion
+
+        #region METODOS
+        /// <summary>
+        /// Retorna los datos completos de la mascoto
+        /// </summary>
+        /// <returns></returns>
+        protected virtual string DatosCompletos ()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0} - {1}", Nombre, Raza);
+            return sb.ToString();
+        }
+
+        protected abstract string Ficha ();
+        
+           
+        
+
+        #endregion
+
+        #region SOBRECARGAS
+
+        /// <summary>
+        /// Compara si son iguales de nombre y raza, de ser asi retorna TRUE,
+        /// False si no. 
+        /// </summary>
+        /// <param name="m1"></param>
+        /// <param name="m2"></param>
+        /// <returns></returns>
+        public static bool operator == (Mascota m1, Mascota m2)
+        {
+            if (m1.Nombre == m2.Nombre && m1.Raza == m2.Raza)
+            {
+                return true;
+            }
+                return false;
+        }
+
+        public static bool operator != (Mascota m1, Mascota m2)
+        {
+            return !(m1 == m2);
+        }
+        #endregion
+    }
+}
